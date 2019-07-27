4a7b6025954d25a9247acd2d12698fa5a7faad8b
ankitesh <ankitesh4@gmail.com>
2019-07-28 01:20:38 +0530

    Completed basic crud in react and redux

--- src/Component/App.js
+++ src/Component/App.js
@@ -1,27 +1,27 @@
 import React from "react";
-import history from '../history';
-import { Router, Route } from "react-router-dom";
+import history from "../history";
+import { Router, Route, Switch } from "react-router-dom";
 
-import StreamCreate from '../Component/streams/StreamCreate';
-import StreamDelete from '../Component/streams/StreamDelete';
-import StreamEdit from '../Component/streams/StreamEdit';
-import StreamList from '../Component/streams/StreamList';
-import StreamShow from '../Component/streams/StreamShow';
+import StreamCreate from "../Component/streams/StreamCreate";
+import StreamDelete from "../Component/streams/StreamDelete";
+import StreamEdit from "../Component/streams/StreamEdit";
+import StreamList from "../Component/streams/StreamList";
+import StreamShow from "../Component/streams/StreamShow";
 
-import Header from './Header';
-
+import Header from "./Header";
 
 const App = () => {
   return (
     <Router history={history}>
       <div>
         <Header />
-        <Route path="/" exact component={StreamList} />
-        <Route path="/streams/new" exact component={StreamCreate} />
-        <Route path="/streams/edit/:id" exact component={StreamEdit} />
-        <Route path="/streams/delete/:id" exact component={StreamDelete} />
-        <Route path="/streams/show" exact component={StreamShow} />
-
+        <Switch>
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit/:id" exact component={StreamEdit} />
+          <Route path="/streams/delete/:id" exact component={StreamDelete} />
+          <Route path="/streams/:id" exact component={StreamShow} />
+        </Switch>
       </div>
     </Router>
   );
